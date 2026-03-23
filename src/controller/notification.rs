b9ddabaeab6461a351f51dc091f5d9afc7738ef8
nayraarr <zita.nayra.ardini@gmail.com>
2026-03-24 01:36:57 +0700

    Create Notification service struct skeleton.

---
+++ src/controller/notification.rs
@@ -0,0 +1,6 @@
+use rocket::response::status::Created;
+use rocket::serde::json::Json;
+
+use bambangshop::Result;
+use crate::model::subscriber::Subscriber;
+use crate::model::notification::Notification;
